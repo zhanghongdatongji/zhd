bcc9ea046f449ca203900ad97facac3dd242186a
zhanghongdatongji <18916226159@163.com>
2018-07-28 14:26:21 +0800

    ex13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+cats=float(raw_input("Please input cats: "))
+dogs=float(raw_input("Please input dogs: "))
+if cats<dogs:
+    print"more dogs than cats"
+elif dogs<cats:
+    print"more cats than dogs"
+else:
+    print"sae number of cats and dogs"
